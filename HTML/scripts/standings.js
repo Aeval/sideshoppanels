54be78ab58bcc8c1c4fb427b41221700eb49e84f
Zackary Fisher <zfisher@thorlabs.com>
2018-08-21 12:09:17 -0400

    changed some shit, mainly adding pics and making the data call actually work

--- HTML/scripts/standings.js
+++ HTML/scripts/standings.js
@@ -1,24 +1,53 @@
 function getStandings() {
-    $.get('http://www.dota2.com/international/getgroupstagestandings/?l=english')
-        .done(function(data){
-            var groupA = data[0];
-            var groupB = data[1];
-            var standingsA = groupA['team_standings'];
-            var standingsB = groupB['team_standings'];
+    const teams = {
+        Liquid: '2163',
+        EG: '39',
+        VP: '1883502',
+        VGjStorm: '5228654',
+        Fnatic: '350190',
+        Winstrike: '5229127',
+        LGD: '15',
+        OG: '2586976',
+        Mineski: '543897',
+        IG: '5',
+        Optic: '5026801',
+        TNC: '2108395',
+        VG: '726228',
+        Secret: '1838315'
+    };
 
-            $('#groupA').empty();
-            $('#groupB').empty();
-            
-            standingsA.forEach(team => {
-                $('#groupA').append("<tr><td>"+standingsA[team].team_id+"</td><td>"+standingsA[team].team_name+"</td><td>" + standingsA[team].match_wins + "</td><td>" + standingsA[team].match_losses + "</td></tr>");
-            }); 
+    $('#groupA').empty();
+    $('#groupB').empty();
 
-            standingsB.forEach(team => {
-                $('#groupB').append("<tr><td>"+standingsB[team].team_id+"</td><td>"+standingsB[team].team_name+"</td><td>" + standingsB[team].match_wins + "</td><td>" + standingsB[team].match_losses + "</td></tr>");
+    $.each(teams, function(key, value) {
+        $.get(`https://api.opendota.com/api/teams/${value}/matches`)
+            .done(function(data){
+                let gamesWon = 0;
+                let gamesLost = 0;
+                
+                for (let i = 0; i < 30; i++) {
+                    let game = data[i];
+                    
+                    if(game.start_time > 1534291200 && game.leagueid == 9870){
+                        if((game.radiant == true && game.radiant_win == true)||(game.radiant == false && game.radiant_win == false)){
+                            gamesWon += 1;
+                        }else{
+                            gamesLost += 1;
+                        }
+                    }
+                }
+                
+                if(key < 'Oo'){
+                    $('#groupA').append("<tr><td><img src='../Img2/teams/"+value+".png' height='40' width='40'></img></td><td>"+ key +"</td><td>" + gamesWon + "</td><td>" + gamesLost + "</td></tr>");
+                }else{
+                    $('#groupB').append("<tr><td><img src='../Img2/teams/"+value+".png' height='40' width='40'></img></td><td>"+ key +"</td><td>" + gamesWon + "</td><td>" + gamesLost + "</td></tr>");
+                }
+                
             });
-        });
-}
+    });
+};
 
 $(document).ready(function() {
-    var getData = setInterval(getStandings, 30000);
+    getStandings();
+    var getData = setInterval(getStandings, 900000);
 });
